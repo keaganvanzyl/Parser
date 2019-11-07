3b9e2a4525e916c5cc5c0e727a411ae183bd2e5a
Keagan van Zyl <keagan.vanzyl@gmail.com>
2019-11-07 15:23:20 +0000

    Added a method that plots stats about the playlists

--- main.py
+++ main.py
@@ -72,3 +72,26 @@ def findCommonTracks(Filenames):
                 "Track names written to common.txt " % len(commonTracks))
     else:
         print("No common tracks")
+
+
+def plotStats(fileName):
+    # read in a playlist
+    plist = plistlib.readPlist(fileName)
+    # get the tracks from the playlist
+    tracks = plist['Tracks']
+    # create lists of song ratings are track durations
+    ratings = []
+    durations = []
+    # iterate through the tracks
+    for trackId, track in tracks.items():
+        try:
+            ratings.append(track['Album Rating'])
+            durations.append(track['Total Time'])
+        except:
+            # ignore
+            pass
+
+    # ensure that valid data was collected
+    if ratings == [] or durations == []:
+        print("No valid Album Rating/Total Time data in %s." % fileName)
+        return
