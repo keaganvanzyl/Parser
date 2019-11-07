672fa3d8c25c3a5869a8b12e1893de666b5a5058
Keagan van Zyl <keagan.vanzyl@gmail.com>
2019-11-07 14:56:05 +0000

    Added findCommonTracks method, takes list of filenames and prints number

--- main.py
+++ main.py
@@ -38,3 +38,37 @@ def findDuplicates(FileName):
     for val in dups:
         f.write("[%d] %s\n" % (val[0], val[1]))
     f.close()
+
+def findCommonTracks(Filenames):
+    # a list of sets of track trackNames
+    trackNameSets = []
+    for fileName in fileNames:
+        #create a ew set
+        trackNames = set()
+        #read in playlist
+        plist = plistlib.readPlist(fileName)
+        # get the tracks
+        tracks = plist['Tracks']
+        # iterate through the tracks
+        for trackId, track in tracks.items():
+            try:
+                # add the track name to a set
+                trackNames.add(track['Name'])
+            except:
+                # ignore
+                pass
+        # add to list
+        trackNameSets.append(trackNames)
+    # get the set of common trcks
+    commonTracks = set.intersection(*trackNameSets)
+    # write to file
+    if len(commonTracks) > 0:
+        f = open("common.txt", "w")
+        for val in commonTracks:
+            s = "%s\n" % val
+            f.write(s.encode("UTF-8"))
+        f.close()
+        print("%d common tracks found. "
+                "Track names written to common.txt " % len(commonTracks))
+    else:
+        print("No common tracks")
